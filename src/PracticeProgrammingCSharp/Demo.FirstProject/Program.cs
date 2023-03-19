10da524e43998a9316786f5331611c0c28c90b6a
Ayush <abhuiyanshrizoy@gmail.com>
2023-03-19 21:53:33 +0600

    PracticeProgrammingCSharp Solution Initialized

---
+++ src/PracticeProgrammingCSharp/Demo.FirstProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
